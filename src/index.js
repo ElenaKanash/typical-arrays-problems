31fda75d1f958debbac4ac386d3f1a6f1ca197a3
Elena Kanash <hird250@gmail.com>
2021-03-16 01:54:41 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (typeof  array === 'undefined' ||array.length == 0 ||  array.length == null)  {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array === 'undefined' ||array.length == 0 ||  array.length == null) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
+        }
+        return max;
+    }
+};
+
+exports.avg = function avg(array) {
+    if (typeof array === 'undefined' ||array.length == 0 ||  array.length == null)  {
+        return 0;
+    } else {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
